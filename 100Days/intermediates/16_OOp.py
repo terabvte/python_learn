e38ab63863f2d83351d9c18ca1f4c1e82acb66d7
terabvte <marcofediuc@gmail.com>
2024-10-18 16:06:54 +0200

    Learn package managing and PrettyTable

--- 100Days/intermediates/16_OOp.py
+++ 100Days/intermediates/16_OOp.py
@@ -1,13 +1,24 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
-# Init Turtle object
-jake = Turtle()
-jake.shape("turtle")
-jake.color("chartreuse4")
-# Init Screen
-my_screen = Screen()
-print(my_screen.canvheight)
-# Move jake by 100 paces forward
-jake.forward(100)
+# # Init Turtle object
+# jake = Turtle()
+# jake.shape("turtle")
+# jake.color("chartreuse4")
+# # Init Screen
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# # Move jake by 100 paces forward
+# jake.forward(100)
 
-my_screen.exitonclick()
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+
+print(table)
