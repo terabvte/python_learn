0b640a6b8f91bc967bf27d1c445f458f3561ca39
terabvte <marcofediuc@gmail.com>
2024-10-18 15:24:37 +0200

    Fix folder structure

---
+++ 100Days/intermediates/16_OOp.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+
+# Init Turtle object
+jake = Turtle()
+jake.shape("turtle")
+jake.color("chartreuse4")
+# Init Screen
+my_screen = Screen()
+print(my_screen.canvheight)
+# Move jake by 100 paces forward
+jake.forward(100)
+
+my_screen.exitonclick()
+
+
+
