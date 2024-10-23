3d146747c4eb54cf0728440b98aa1aa798edff06
terabvte <marcofediuc@gmail.com>
2024-10-23 10:42:22 +0200

    Enable quiz game to use questions from TriviaDB

--- 100Days/intermediates/day-17/quiz-game/main.py
+++ 100Days/intermediates/day-17/quiz-game/main.py
@@ -1,12 +1,15 @@
+import html
+
 from data import question_data
 from quiz_brain import QuizBrain
 from question_model import Question
 
-
 question_bank = []
 
 for question in question_data:
-    question_bank.append(Question(question["text"], question["answer"]))
+    question_bank.append(
+        Question(html.unescape(question["question"]), question["correct_answer"])
+    )
 
 brain = QuizBrain(question_bank)
 
--- 100Days/intermediates/day-17/quiz-game/question_model.py
+++ 100Days/intermediates/day-17/quiz-game/question_model.py
@@ -1,4 +1,4 @@
 class Question:
-    def __init__(self, text, answer):
-        self.text = text
-        self.answer = answer
+    def __init__(self, question, correct_answer):
+        self.text = question
+        self.answer = correct_answer
