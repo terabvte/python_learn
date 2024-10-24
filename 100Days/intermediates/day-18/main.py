87cdbee45e1ae4d027711c2a4510eefac4861c10
terabvte <marcofediuc@gmail.com>
2024-10-24 10:29:44 +0200

    Turtle GUI - Multipolygon challenge

--- 100Days/intermediates/day-18/main.py
+++ 100Days/intermediates/day-18/main.py
@@ -1,13 +1,58 @@
 from turtle import Turtle, Screen
-
+import random
 turtle = Turtle()
 screen = Screen()
 
-for i in range(15):
-    turtle.fd(10)
-    turtle.up()
-    turtle.fd(10)
-    turtle.pd()
+colors = [
+    "alice blue", "antique white", "aqua", "aquamarine", "azure", "beige", 
+    "bisque", "black", "blanched almond", "blue", "blue violet", "brown", 
+    "burlywood", "cadet blue", "chartreuse", "chocolate", "coral", 
+    "cornflower blue", "cornsilk", "crimson", "cyan", "dark blue", 
+    "dark cyan", "dark goldenrod", "dark gray", "dark green", "dark khaki", 
+    "dark magenta", "dark olive green", "dark orange", "dark orchid", 
+    "dark red", "dark salmon", "dark sea green", "dark slate blue", 
+    "dark slate gray", "dark turquoise", "dark violet", "deep pink", 
+    "deep sky blue", "dim gray", "dodger blue", "firebrick", "floral white", 
+    "forest green", "gainsboro", "ghost white", "gold", "goldenrod", 
+    "gray", "green", "green yellow", "honeydew", "hot pink", "indian red", 
+    "indigo", "ivory", "khaki", "lavender", "lavender blush", "lawn green", 
+    "lemon chiffon", "light blue", "light coral", "light cyan", 
+    "light goldenrod yellow", "light gray", "light green", "light pink", 
+    "light salmon", "light sea green", "light sky blue", "light slate gray", 
+    "light steel blue", "light yellow", "lime", "lime green", 
+    "linen", "magenta", "maroon", "medium aquamarine", "medium blue", 
+    "medium orchid", "medium purple", "medium sea green", "medium slate blue", 
+    "medium spring green", "medium turquoise", "medium violet red", 
+    "midnight blue", "mint cream", "misty rose", "moccasin", "navajo white", 
+    "navy", "old lace", "olive", "olive drab", "orange", "orange red", 
+    "orchid", "pale goldenrod", "pale green", "pale turquoise", 
+    "pale violet red", "papaya whip", "peach puff", "peru", "pink", 
+    "plum", "powder blue", "purple", "red", "rosy brown", "royal blue", 
+    "saddle brown", "salmon", "sandy brown", "sea green", "seashell", 
+    "sienna", "silver", "sky blue", "slate blue", "slate gray", 
+    "snow", "spring green", "steel blue", "tan", "teal", "thistle", 
+    "tomato", "turquoise", "violet", "wheat", "white", "white smoke", 
+    "yellow", "yellow green"
+]
 
+# Dotted line
+# for i in range(15):
+#     turtle.fd(10)
+#     turtle.up()
+#     turtle.fd(10)
+#     turtle.pd()
 
+turtle.width(5)
+
+# Multi polygon challenge
+polygons = [3, 4, 5, 6, 7, 8, 9, 10]
+for shape in polygons:
+    angle = 360 / shape
+    turtle.color(random.choice(colors))
+    i = shape
+    while i != 0:
+        turtle.fd(100)
+        turtle.rt(angle)
+        i -= 1
+     
 screen.exitonclick()
