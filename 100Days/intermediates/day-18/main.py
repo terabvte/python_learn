4b7eae39129ff4ebd98be268f8e49a6a9a579971
terabvte <marcofediuc@gmail.com>
2024-10-24 11:49:05 +0200

    Turtle GUI - Optimize random color picking

--- 100Days/intermediates/day-18/main.py
+++ 100Days/intermediates/day-18/main.py
@@ -1,149 +1,157 @@
-from turtle import Turtle, Screen
+import turtle as t
+from turtle import Screen
 import random
 
-tt = Turtle()
+tt = t.Turtle()
 screen = Screen()
+t.colormode(255)
+# colors = [
+#     "alice blue",
+#     "antique white",
+#     "aqua",
+#     "aquamarine",
+#     "azure",
+#     "beige",
+#     "bisque",
+#     "blanched almond",
+#     "blue",
+#     "blue violet",
+#     "brown",
+#     "burlywood",
+#     "cadet blue",
+#     "chartreuse",
+#     "chocolate",
+#     "coral",
+#     "cornflower blue",
+#     "cornsilk",
+#     "crimson",
+#     "cyan",
+#     "dark blue",
+#     "dark cyan",
+#     "dark goldenrod",
+#     "dark gray",
+#     "dark green",
+#     "dark khaki",
+#     "dark magenta",
+#     "dark olive green",
+#     "dark orange",
+#     "dark orchid",
+#     "dark red",
+#     "dark salmon",
+#     "dark sea green",
+#     "dark slate blue",
+#     "dark slate gray",
+#     "dark turquoise",
+#     "dark violet",
+#     "deep pink",
+#     "deep sky blue",
+#     "dim gray",
+#     "dodger blue",
+#     "firebrick",
+#     "floral white",
+#     "forest green",
+#     "gainsboro",
+#     "ghost white",
+#     "gold",
+#     "goldenrod",
+#     "gray",
+#     "green",
+#     "green yellow",
+#     "honeydew",
+#     "hot pink",
+#     "indian red",
+#     "indigo",
+#     "ivory",
+#     "khaki",
+#     "lavender",
+#     "lavender blush",
+#     "lawn green",
+#     "lemon chiffon",
+#     "light blue",
+#     "light coral",
+#     "light cyan",
+#     "light goldenrod yellow",
+#     "light gray",
+#     "light green",
+#     "light pink",
+#     "light salmon",
+#     "light sea green",
+#     "light sky blue",
+#     "light slate gray",
+#     "light steel blue",
+#     "light yellow",
+#     "lime",
+#     "lime green",
+#     "linen",
+#     "magenta",
+#     "maroon",
+#     "medium aquamarine",
+#     "medium blue",
+#     "medium orchid",
+#     "medium purple",
+#     "medium sea green",
+#     "medium slate blue",
+#     "medium spring green",
+#     "medium turquoise",
+#     "medium violet red",
+#     "midnight blue",
+#     "mint cream",
+#     "misty rose",
+#     "moccasin",
+#     "navajo white",
+#     "navy",
+#     "old lace",
+#     "olive",
+#     "olive drab",
+#     "orange",
+#     "orange red",
+#     "orchid",
+#     "pale goldenrod",
+#     "pale green",
+#     "pale turquoise",
+#     "pale violet red",
+#     "papaya whip",
+#     "peach puff",
+#     "peru",
+#     "pink",
+#     "plum",
+#     "powder blue",
+#     "purple",
+#     "red",
+#     "rosy brown",
+#     "royal blue",
+#     "saddle brown",
+#     "salmon",
+#     "sandy brown",
+#     "sea green",
+#     "seashell",
+#     "sienna",
+#     "silver",
+#     "sky blue",
+#     "slate blue",
+#     "slate gray",
+#     "snow",
+#     "spring green",
+#     "steel blue",
+#     "tan",
+#     "teal",
+#     "thistle",
+#     "tomato",
+#     "turquoise",
+#     "violet",
+#     "wheat",
+#     "white",
+#     "white smoke",
+#     "yellow",
+#     "yellow green",
+# ]
 
-colors = [
-    "alice blue",
-    "antique white",
-    "aqua",
-    "aquamarine",
-    "azure",
-    "beige",
-    "bisque",
-    "blanched almond",
-    "blue",
-    "blue violet",
-    "brown",
-    "burlywood",
-    "cadet blue",
-    "chartreuse",
-    "chocolate",
-    "coral",
-    "cornflower blue",
-    "cornsilk",
-    "crimson",
-    "cyan",
-    "dark blue",
-    "dark cyan",
-    "dark goldenrod",
-    "dark gray",
-    "dark green",
-    "dark khaki",
-    "dark magenta",
-    "dark olive green",
-    "dark orange",
-    "dark orchid",
-    "dark red",
-    "dark salmon",
-    "dark sea green",
-    "dark slate blue",
-    "dark slate gray",
-    "dark turquoise",
-    "dark violet",
-    "deep pink",
-    "deep sky blue",
-    "dim gray",
-    "dodger blue",
-    "firebrick",
-    "floral white",
-    "forest green",
-    "gainsboro",
-    "ghost white",
-    "gold",
-    "goldenrod",
-    "gray",
-    "green",
-    "green yellow",
-    "honeydew",
-    "hot pink",
-    "indian red",
-    "indigo",
-    "ivory",
-    "khaki",
-    "lavender",
-    "lavender blush",
-    "lawn green",
-    "lemon chiffon",
-    "light blue",
-    "light coral",
-    "light cyan",
-    "light goldenrod yellow",
-    "light gray",
-    "light green",
-    "light pink",
-    "light salmon",
-    "light sea green",
-    "light sky blue",
-    "light slate gray",
-    "light steel blue",
-    "light yellow",
-    "lime",
-    "lime green",
-    "linen",
-    "magenta",
-    "maroon",
-    "medium aquamarine",
-    "medium blue",
-    "medium orchid",
-    "medium purple",
-    "medium sea green",
-    "medium slate blue",
-    "medium spring green",
-    "medium turquoise",
-    "medium violet red",
-    "midnight blue",
-    "mint cream",
-    "misty rose",
-    "moccasin",
-    "navajo white",
-    "navy",
-    "old lace",
-    "olive",
-    "olive drab",
-    "orange",
-    "orange red",
-    "orchid",
-    "pale goldenrod",
-    "pale green",
-    "pale turquoise",
-    "pale violet red",
-    "papaya whip",
-    "peach puff",
-    "peru",
-    "pink",
-    "plum",
-    "powder blue",
-    "purple",
-    "red",
-    "rosy brown",
-    "royal blue",
-    "saddle brown",
-    "salmon",
-    "sandy brown",
-    "sea green",
-    "seashell",
-    "sienna",
-    "silver",
-    "sky blue",
-    "slate blue",
-    "slate gray",
-    "snow",
-    "spring green",
-    "steel blue",
-    "tan",
-    "teal",
-    "thistle",
-    "tomato",
-    "turquoise",
-    "violet",
-    "wheat",
-    "white",
-    "white smoke",
-    "yellow",
-    "yellow green",
-]
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    colors = (r, g, b)
+    return colors
 
 # Dotted line
 # for i in range(15):
@@ -168,25 +176,27 @@ tt.color("white")
 #         i -= 1
 print(random.randint(0, 3))
 
-while True:
+# My take on random walk:
+# while True:
 
-    direction = random.randint(0, 3)
+#     direction = random.randint(0, 3)
 
-    if direction == 0:
-        tt.fd(25)
-        tt.rt(90)
-        tt.color(random.choice(colors))
-    elif direction == 1:
-        tt.bk(25)
-        tt.rt(90)
-        tt.color(random.choice(colors))
-    # elif direction == 2:
-    #     tt.rt(25)
-    #     tt.rt(90)
-    #     tt.color(random.choice(colors))
-    # elif direction == 3:
-    #     tt.lt(25)
-    #     tt.rt(90)
-    #     tt.color(random.choice(colors))
+#     if direction == 0:
+#         tt.fd(25)
+#         tt.rt(90)
+#         tt.color(random.choice(colors))
+#     elif direction == 1:
+#         tt.bk(25)
+#         tt.rt(90)
+#         tt.color(random.choice(colors))
+
+# Random walk solution:
+directions = [0, 90, 180, 270]
+
+for i in range(200):
+    tt.color(random_color())
+    tt.fd(30)
+    tt.setheading(random.choice(directions))
+
 
 screen.exitonclick()
