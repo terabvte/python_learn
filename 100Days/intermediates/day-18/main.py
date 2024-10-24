de369d4088f5553bc5c80b97f7bce6e012a1a201
terabvte <marcofediuc@gmail.com>
2024-10-24 09:36:02 +0200

    Start Day 18 - Turtle GUI; Draw square

---
+++ 100Days/intermediates/day-18/main.py
@@ -0,0 +1,23 @@
+from turtle import Turtle, Screen
+
+turtle = Turtle()
+screen = Screen()
+
+
+
+turtle.shape("arrow")
+turtle.color("black")
+
+for i in range (4):
+    turtle.fd(100)
+    turtle.rt(90)
+
+
+
+
+
+
+
+
+screen.exitonclick()
+
