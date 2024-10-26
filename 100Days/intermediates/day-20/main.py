7f432d978e335bf9a194821796cac1367278529b
terabvte <marcofediuc@gmail.com>
2024-10-26 11:28:24 +0200

    Day 20 - Enable movement and optimize segment positioning

--- 100Days/intermediates/day-20/main.py
+++ 100Days/intermediates/day-20/main.py
@@ -1,24 +1,64 @@
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(600, 600, 650, 250)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
+
+
+def up():
+    snake[0].seth(90)
+
+
+def down():
+    snake[0].seth(270)
+
+
+def left():
+    snake[0].seth(0)
+
+
+def right():
+    snake[0].seth(180)
+
 
 # Step 1: Create Snake Body
 
-snakes = [Turtle() for i in range(3)]
+snake = [Turtle() for i in range(60)]
 last_position = [0, 0]
-for snake in snakes:
-    snake.shape("square")
-    snake.color("white")
-    snake.pu()
-    snake.shapesize(0.5, 0.5, 0.5)
-
-    snake.goto(last_position)
+for segment in snake:
+    segment.shape("square")
+    segment.color("white")
+    segment.pu()
+    segment.shapesize(0.5, 0.5, 0.5)
+    segment.speed(1)
+    segment.goto(last_position)
     last_position[0] -= 10
     # start_line[1] += 45
 
+screen.update()
+
+# Step 2: Animating snake segments
+
+i = 0
+
+screen.listen()
+screen.onkey(right, "a")
+screen.onkey(left, "d")
+screen.onkey(up, "w")
+screen.onkey(down, "s")
+
+while True:
+    screen.update()
+    time.sleep(0.05)
+
+    positions = [seg.pos() for seg in snake]
+    for i in range(len(snake) - 1, 0, -1):
+        snake[i].goto(positions[i - 1])
+
+    snake[0].forward(10)
 
 
 screen.exitonclick()
