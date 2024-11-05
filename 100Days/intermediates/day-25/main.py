234f83f7cb4a489857a148f7b6ef57a60bfd70b6
terabvte <marcofediuc@gmail.com>
2024-11-05 23:06:01 +0100

    I've no clue what this is but I'm committing lol

--- 100Days/intermediates/day-25/main.py
+++ 100Days/intermediates/day-25/main.py
@@ -3,16 +3,19 @@
 #     data = [line.rstrip() for line in weather_conditions]
 # print(data)
 
-import csv
+# import csv
 
-with open("./100Days/intermediates/day-25/weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    for row in data:
-        temperatures.append(row[1])
-        # print(row)
-    temperatures.pop(0)
-    temperatures = [int(value) for value in temperatures]
-    print(temperatures)
-    
-    #fe
+# with open("./100Days/intermediates/day-25/weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         temperatures.append(row[1])
+#         # print(row)
+#     temperatures.pop(0)
+#     temperatures = [int(value) for value in temperatures]
+#     print(temperatures)
+
+import pandas as pd
+
+df = pd.read_csv("./100Days/intermediates/day-25/weather_data.csv")
+print(df["temp"])
