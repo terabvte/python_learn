604da264573221820524e8977da345ff426938b5
terabvte <marcofediuc@gmail.com>
2024-11-11 17:15:47 +0100

    Initalize files for Day 27

--- 100Days/intermediates/day-25/us-states-game/main.py
+++ 100Days/intermediates/day-25/us-states-game/main.py
@@ -1,24 +1,17 @@
 from turtle import Turtle, Screen
 import pandas as pd
 
-class Writer(Turtle):
-    def __init__(self):
-        super().__init__()
-        self.pu()
-
-    def writes(self, x, y, state):
-        self.goto(x, y)
-        self.write(state)
-
 
 turtle = Turtle()
-screen = turtle.Screen()
+screen = Screen()
 screen.setup(700, 500, 650, 250)
 screen.title("U.S. States Game")
 image = "./100Days/intermediates/day-25/us-states-game/blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
-write = Writer()
+write = Turtle()
+write.pu()
+write.hideturtle()
 
 def get_mouse_click_coor(x, y):
     print(x, y)
@@ -29,16 +22,19 @@ df = pd.read_csv("./100Days/intermediates/day-25/us-states-game/50_states.csv")
 
 states_list = df.values.tolist()
 # print(states_list)
-# print(states_list)
 
 score = 0
-
 while score != 50:
-    answer = screen.textinput(title="Guess the State", prompt="What's another state's name?").title()
+    answer = screen.textinput(title=f"Guess the State ({score}/50)", prompt="What's another state's name?").title()
 
-    if answer == states_list[0:51]:
-        write.writes()
-
+    for state in states_list:
+        if answer == state[0]:
+            x, y = state[1], state[2]
+            write.goto(x, y)
+            write.write(answer)
+            print("yeah that one's in")
+            score += 1
+            break
 
 turtle.onscreenclick(get_mouse_click_coor)
 turtle.mainloop()
