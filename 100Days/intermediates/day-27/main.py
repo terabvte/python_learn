5994239863a5dcee0e407b01880ca29b16b07351
terabvte <marcofediuc@gmail.com>
2024-11-11 18:48:54 +0100

    Find out more about Tkinter; grid, place and such

--- 100Days/intermediates/day-27/main.py
+++ 100Days/intermediates/day-27/main.py
@@ -1,16 +1,33 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI")
-window.minsize(width=800, height=600)
+window.minsize(width=500, height=300)
+window.config(padx=100, pady=200)
 
 # Label
 
-label = tkinter.Label(text="I'm a Label", font=("Arial", 24, "bold"))
-label.pack(side="right")
+label = Label(text="I'm a Label", font=("Arial", 24, "bold"))
+# label.pack(side="right")
+label.grid(column=0, row=0)
+label.config(text="New Text")
+# label.pack(side="top")
+
+# Button
+def button_clicked():
+    label.config(text=input.get(), pady=20)
 
 
+button = Button(text="Clicker")
+button.grid(column=1, row=1)
 
+
+new_button = Button(text="NEWY Clicker")
+new_button.grid(column=2, row=0)
+# Entry
+
+input = Entry(width=10)
+input.grid(column=3, row=2)
 
 
 
