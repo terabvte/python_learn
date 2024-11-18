c99a939cc4fd427f5338218e043f279fc8c30994
terabvte <marcofediuc@gmail.com>
2024-11-18 10:39:55 +0100

    Finish OWM Challenge with Twilio API for SMS messaging

--- 100Days/intermediates/day-35/main.py
+++ 100Days/intermediates/day-35/main.py
@@ -1,7 +1,13 @@
 import requests
-MY_LAT = 43.591560
-MY_LON = 10.596240
-API_KEY = "0f3f21e452f8073b8ca75787534afc07"
+from twilio.rest import Client
+MY_LAT = 43.591560 # 43.591560
+MY_LON = 10.596240 # 10.596240
+API_KEY = "cens"
+
+account_sid = 'cens'
+auth_token = 'cens'
+
+# print(message.sid)
 
 # http://api.openweathermap.org/data/2.5/weather?lat=43.591560&lon=10.596240&appid=0f3f21e452f8073b8ca75787534afc07
 
@@ -16,5 +22,31 @@ response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", 
 response.raise_for_status()
 data = response.json()
 
-print(data["cod"])
-print(data)
+# print(data["cod"])
+# print(data)
+
+w_conditions = []
+
+# w_time = data["list"][0]["dt_txt"]
+# w_ID = data["list"][0]["weather"][0]["id"]
+
+for forecast in range(0,4):
+    print(data["list"][forecast]["dt_txt"])
+    w_conditions.append(data["list"][forecast]["weather"][0]["id"])
+    w_conditions.append(data["list"][forecast]["weather"][0]["id"])
+
+for forecast in w_conditions:
+    if forecast < 700:
+        print("Bring an Umbrella")
+        client = Client(account_sid, auth_token)
+        message = client.messages.create(
+          messaging_service_sid='MG7a38773421f12ecc1f865181176194d8',
+          body='Bring an Umbs my man ☔️',
+        #   from_="cens"
+          to='cens'
+        )
+        print(message.status)
+        break
+
+
+# print(w_conditions)
