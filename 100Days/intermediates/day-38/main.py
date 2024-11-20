48b0b7aa56c686ce459ddd92c4a4ada5854d7ff1
terabvte <marcofediuc@gmail.com>
2024-11-20 11:29:29 +0100

    Complete Day 38 challenge of Workout Tracker

--- 100Days/intermediates/day-38/main.py
+++ 100Days/intermediates/day-38/main.py
@@ -1,8 +1,8 @@
 import requests
 from datetime import datetime
 
-NUT_APP_ID = "de12f083"
-NUT_API_KEY = "8cfb569db17469b56f9e0a80a0887f82"
+NUT_APP_ID = "cens"
+NUT_API_KEY = "cens"
 
 nut_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
@@ -15,20 +15,33 @@ query = input("Tell me which exercises you did: ")
 
 nut_parameters = {
     "query": query,
-    "weight_kg": 88.3,
-    "height_cm": 170,
-    "age": 20
 }
 
-response = requests.post(url=nut_endpoint, headers=nut_header, json=nut_parameters)
-response = response.json()
+nut_response = requests.post(url=nut_endpoint, headers=nut_header, json=nut_parameters)
+nut_response = nut_response.json()
 # print(response.text)
-print(response)
+# print(nut_response)
 
 now = datetime.now()
 date = now.strftime("%d/%m/%Y")
 time = now.strftime("%H:%M:%S")
 
-entry = [date, time, response["exercises"][0]["name"].title(), response["exercises"][0]["duration_min"], response["exercises"][0]["nf_calories"]]
+entry = [date, time, nut_response["exercises"][0]["name"].title(), nut_response["exercises"][0]["duration_min"], nut_response["exercises"][0]["nf_calories"]]
 
 print(entry)
+
+
+sheety_endpoint = "cens"
+sheety_parameters = {
+    "workout": {
+        "date": entry[0],
+        "time": entry[1],
+        "exercise": entry[2],
+        "duration": entry[3],
+        "calories": entry[4],
+    }
+}
+
+sheety_request = requests.post(url=sheety_endpoint, json=sheety_parameters)
+print(sheety_request.text)
+sheety_request.raise_for_status()
